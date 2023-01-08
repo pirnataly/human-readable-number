bd43524a2e92019e55b1d26fbee6ef74a69d1ab3
pirnataly <piroznatal@gmail.com>
2023-01-08 21:46:06 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
+  const units =['zero','one','two','three','four','five','six','seven','eight','nine'];
+  const top_ten = ['ten','eleven', 'twelve','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens=['twenty', 'thirty', 'forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const hundreds=['one hundred', 'two hundred', 'three hundred','four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+
 }
